de36301c4026b0816ea6d111dfc55b67a29aa3c3
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2020-07-06 02:18:05 +0530

    Variable Basic

---
+++ src/github.com/variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	x int = 1
+	y int = 2
+)
+
+func main() {
+	var i int = 54
+	a := 45
+	var j int
+	j = 22
+	str := "sai"
+	fmt.Println(i, a, j, str, x)
+	fmt.Printf("%v, %T", a, a)
+}
